748ee6a3cdeef9986aedafaf711c2dfe5aa7cb1d
Dimiqhz <dimiqhz@gmail.com>
2024-12-13 10:38:01 +0300

    Initial Main file

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+from colorama import Init, Fore
+
+def main():
+    st_msg = str(input(Fore.LIGHTBLUE_EX + "→" + Fore.WHITE + "| Enter the path to your SQL file: "))
+
+if __name__ == "__main__":
+    main()
